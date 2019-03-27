af21f556d87224844dbccda77f591ad2f22e0cb2
jinbin <joysmshr@126.com>
2019-03-27 21:32:22 +0800

    Motoserver api with express-mongoose-node

--- main.ts
+++ main.ts
@@ -1,27 +1,61 @@
+import * as dotenv from 'dotenv'; // Loads environment variables from .env file
 import * as express from 'express';
-const app = express();
-const db = require('./model/db');
-const cors = require('cors');
-
-import * as Index from './routes/index';
-
+import * as morgan from 'morgan';  // HTTP request logger middleware for node.js
+import * as mongoose from 'mongoose';
 import * as path from 'path';
+import * as cors from 'cors'; // enable CORS
+import * as bodyParser from 'body-parser';
 
+import setRoutes from './routes/index';
+
+const app = express();
+dotenv.config({ path: '.env' });
+app.set('port', (process.env.PORT || 3000));
+
+
+// View engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
-import * as bodyParser from 'body-parser';
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.use('/', Index.router);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
-app.use('/dist', express.static(__dirname + '/dist'));
+// app.use('/node_modules', express.static(__dirname + '/node_modules'));
+// app.use('/dist', express.static(__dirname + '/dist'));
 
-app.listen(3000, () => {
-    console.log(`listening on port 3000`);
-});
+
+// Mongoose connection
+let mongodbURI;
+if (process.env.NODE_ENV === 'test') {
+  mongodbURI = process.env.MONGODB_TEST_URI;
+} else {
+  mongodbURI = process.env.MONGODB_URI;
+  app.use(morgan('dev'));
+}
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongodbURI)
+  .then(db => {
+    console.log('Connected to MongoDB');
+
+    setRoutes(app);
+
+    app.get('/*', function(req, res) {
+      // res.sendFile(path.join(__dirname, '../public/index.html'));
+      res.send({title: 'Expressangular'});
+    });
+
+    if (!module.parent) {
+      app.listen(app.get('port'), () => console.log(`MotoServer listening on port ${app.get('port')}`));
+    }
+  })
+  .catch(err => console.error(err));
+
+  
+export { app };
+
